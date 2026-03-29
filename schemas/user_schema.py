c27734df093157e60f41db16ac3c62cfab4238a7
Steeven24 <loorjins@gmail.com>
2026-03-29 02:00:05 -0500

    Refactor user schema to improve readability and structure

--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -1,10 +1,18 @@
 from pydantic import BaseModel, EmailStr
 
+
 class UserCreate(BaseModel):
     name: str
     email: EmailStr
     password: str
-    
+
+
+class UserUpdate(BaseModel):
+    name: str | None = None
+    email: EmailStr | None = None
+    password: str | None = None
+
+
 class UserResponse(BaseModel):
     id: int
     name: str
