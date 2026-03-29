2f478f3ac0cc8a27db456b81920daa59ed20a8c0
Steeven24 <loorjins@gmail.com>
2026-03-29 11:30:40 -0500

    feat(roles): add role field to user model

--- models/user_model.py
+++ models/user_model.py
@@ -10,3 +10,4 @@ class User(Base):
     name = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
+    role = Column(String, nullable=False, default="seller")
