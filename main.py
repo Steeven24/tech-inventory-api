e31fa404314ba9c79b4672594bb2c8cb019697c5
Steeven24 <loorjins@gmail.com>
2026-03-29 02:18:42 -0500

    feat(auth): register authentication router

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from database import Base, engine
 from models.user_model import User
+from routes.auth_routes import router as auth_router
 from routes.user_routes import router as user_router
 
 app = FastAPI()
@@ -9,6 +10,7 @@ app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
 app.include_router(user_router)
+app.include_router(auth_router)
 
 @app.get("/")
 def root():
